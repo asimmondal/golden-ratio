0bcfe7c41685128d46c612add302779c3c8fa645
TANIGUCHI Makoto <techctaniguchi@h2o-space.net>
2015-05-29 07:12:45 +0000

    build html

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Golden Ratio</title>
+    
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap-theme.min.css" type="text/css" />
+</head>
+<body>
+    <div class="row">
+        <div class="col-xs-8 col-xs-offset-2">
+            <h1>Golden Ratio - 黄金比</h1>
+            
+            <p>テキストフィールドに数字を入力すると、黄金比を計算します。</p>
+            
+            <form>
+                <input type="text" name="num" size="5"/>
+                <input type="submit" class="btn" value="Send"/>
+            </form>
+        
+            <p>の黄金比は・・・ <span></span></p>    
+        </div>
+    </div>
+
+</body>
+</html>
